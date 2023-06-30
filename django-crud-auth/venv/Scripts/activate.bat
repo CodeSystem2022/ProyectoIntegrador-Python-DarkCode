c4dc5048ae7c4755c9dada263ffe6fd974a1a22f
juani sorato <juanisorato17@yahoo.com>
2023-06-30 17:28:45 -0300

    Agrego mi parte del proyecto
    Agrego parte 3 del proyecto

--- django-crud-auth/venv/Scripts/activate.bat
+++ django-crud-auth/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\CesarG\Documents\GitHub\ProyectoIntegrador-Python-DarkCode\django-crud-auth\venv
+set VIRTUAL_ENV=D:\django-crud-auth\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
